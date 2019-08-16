092d2e1d268ccc0c67c167ea8917224997010b15
rosialoasis <2460498901@qq.com>
2019-08-16 14:09:34 +0800

    new create

---
+++ src/main/java/com/example/demo/controller/Demo1Controller.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试控制器
+ *
+ * @author: @rosia
+ * @create: 2018-05-08-下午 16:46
+ */
+@RestController
+public class Demo1Controller {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
